c3f5b99c45e5aeba584103deae1adf4d476974b8
kento tajiri <k.denjiry@gmail.com>
2021-06-07 04:38:28 +0900

    add PartialChart

--- src/parser/chartparser.rs
+++ src/parser/chartparser.rs
@@ -10,7 +10,8 @@ impl Input {
     // Simple parsing function to return just the best node for a given sentence
     fn simple_parse(&self, beam: usize) -> Result<Node, std::io::Error> {
         let lexicon = setup_lexicon(&self.sentence);
-        let chart = chart_accumulator(lexicon, &self.sentence);
+        let chart = chart_parser(lexicon, &self.sentence);
+        unimplemented!();
     }
 
     // removes occurrences of non-letters from an input text.
@@ -20,4 +21,16 @@ impl Input {
 }
 
 type Chart = std::collections::HashMap<(usize, usize), Vec<Node>>;
-fn chart_accumulator(lexicon: LexicalItem, sentence: &str) -> Chart {}
+fn chart_parser(lexicon: LexicalItem, sentence: &str) -> Chart {
+    unimplemented!();
+}
+
+struct PartialChart {
+    chart: Chart,
+    seplist: Vec<usize>,
+    i: usize,
+    stack: String,
+}
+fn chart_accumulator(beam: usize, lexicon: LexicalItem, pc: PartialChart, c: char) -> PartialChart {
+    unimplemented!();
+}
