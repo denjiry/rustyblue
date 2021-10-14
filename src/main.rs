7f15f8cb2839272bc9f0440924c136cc75ed2c46
kento tajiri <k.denjiry@gmail.com>
2021-10-14 10:55:12 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,7 @@ fn main() -> Result<()> {
     let mut lex = String::new();
     json.read_to_string(&mut lex)?;
     let nodes = parse_lexicon(&lex)?;
-    // let mut lexicon = PatriciaMap::from_iter(nodes.into_iter());
-    let mut lexicon = PatriciaMap::new();
-    for n in nodes {
-        lexicon.insert(&n.pf, n);
-    }
+    let map: PatriciaMap<_> = nodes.into_iter().collect();
     let parsed = chartparser::simple_parse(&input, &input, 10);
     dbg!(parsed.unwrap());
     Ok(())
