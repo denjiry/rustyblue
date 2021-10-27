159501b7b74bceb9fee9d78ab3d73a3bdb9c9313
kento tajiri <k.denjiry@gmail.com>
2021-10-27 18:01:10 +0900

    update

--- src/parser/ccg/rules.rs
+++ src/parser/ccg/rules.rs
@@ -1,6 +1,8 @@
 use crate::Node;
+use crate::parser::ccg::unify;
 
 pub fn forward_function_application_rule<'a>(lnode: &Node, rnode: &Node) -> Option<&'a Node> {
+    let new_category = unify::unify_category();
     todo!();
 }
 
--- src/parser/ccg/unify.rs
+++ src/parser/ccg/unify.rs
@@ -1,5 +1,15 @@
 use crate::parser::ccg::Cat;
 
-fn unify_category() -> Option<Cat> {
+/// | Data for category/feature unification
+/// csub :: SubstData Cat
+/// fsub :: SubstData [FeatureValue]
+#[derive(Debug, PartialEq, Eq)]
+enum SubstData<T> {
+    SubstLink(u32),
+    SubstVal(T),
+}
+type Assignment<T> = Vec<(u32, SubstData<T>)>;
+
+pub(crate) fn unify_category() -> Option<Cat> {
     todo!();
 }
